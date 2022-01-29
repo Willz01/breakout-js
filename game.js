58dca16e5e1e9289d48091db37dc2c943fd6b23a
Wills Ekanem <ekanemwills1@gmail.com>
2022-01-29 18:38:22 +0100

    ball trajectory

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector('.grid')
+const score = document.querySelector('.score')
 
 const blkW = 100
 const blkH = 20
@@ -8,6 +9,12 @@ let currentPosition = controllerPOS
 
 const ballStart = [270, 30]
 let ballCurrentPosition = ballStart
+const ballDiameter = 20
+
+let timerID
+let xD = -2
+let yD = 2
+let hits = 0
 
 
 class Block {
@@ -53,30 +60,110 @@ polluteGrid()
 
 const controller = document.createElement('div')
 controller.classList.add('controller')
-draw()
+drawController()
 grid.appendChild(controller)
 
-function draw() {
+function drawController() {
   controller.style.left = currentPosition[0] + 'px'
   controller.style.bottom = currentPosition[1] + 'px'
 }
 
+
 const moveController = (e) => {
   if (e.key === 'ArrowLeft' && currentPosition[0] > 0) {
     //  move controller 10 px left at each keydown
-    currentPosition[0] -= 10
-    draw()
+    currentPosition[0] -= 20
+    drawController()
   } else if (e.key === 'ArrowRight' && currentPosition[0] < 570 - blkW) {
     //  move controller 10 px right at each keydown
-    currentPosition[0] += 10
-    draw()
+    currentPosition[0] += 20
+    drawController()
+  } else if (e.key === ' ') {
+    document.location.reload()
   }
 }
 
 document.addEventListener('keydown', moveController)
 
+
 const ball = document.createElement('div')
 ball.classList.add('ball')
-ball.style.left = ballCurrentPosition[0] + 'px'
-ball.style.bottom = ballCurrentPosition[1] + 'px'
+drawBall()
 grid.appendChild(ball)
+
+function drawBall() {
+  ball.style.left = ballCurrentPosition[0] + 'px'
+  ball.style.bottom = ballCurrentPosition[1] + 'px'
+}
+
+function moveBall() {
+  ballCurrentPosition[0] += xD
+  ballCurrentPosition[1] += yD
+  drawBall()
+  solveCollisions()
+}
+timerID = setInterval(moveBall, 20)
+
+
+function solveCollisions() {
+  for (let i = 0; i < blocks.length; i++) {
+    // block collision
+    if (
+      (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0]) &&
+      ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1])
+    ) {
+      const blks = Array.from(document.querySelectorAll('.block'))
+      blks[i].classList.remove('block')
+      blocks.splice(i, 1)
+      hits++
+      score.innerText = 'Score: ' + hits + ' points'
+      changeTrajectory()
+
+      if (blocks.length === 0) {
+        score.innerText = 'You win!'
+        clearInterval(timerID)
+        document.removeEventListener('keydown', moveController)
+      }
+
+    }
+
+  }
+  // wall collision
+  if (ballCurrentPosition[0] >= (570 - ballDiameter) || ballCurrentPosition[1] >= (300 - ballDiameter)
+    || ballCurrentPosition[0] <= 0
+  ) {
+    changeTrajectory()
+  }
+
+  if (ballCurrentPosition[1] <= 0) {
+    score.innerText = 'You Lose'
+    clearInterval(timerID)
+    document.removeEventListener('keydown', moveController)
+  }
+
+  if ((ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0] < currentPosition[0] + blkW)
+    && (ballCurrentPosition[1] > currentPosition[1] && ballCurrentPosition[1] < currentPosition[1] + blkH)) {
+    changeTrajectory()
+  }
+
+}
+
+function changeTrajectory() {
+  if (xD === 2 && yD === 2) {
+    yD = -2
+    return
+  }
+  if (xD === 2 && yD === -2) {
+    xD = -2
+    return
+  }
+  if (xD === -2 && yD === 2) {
+    xD = 2
+    return
+  }
+  if (xD === -2 && yD === -2) {
+    yD = 2
+    return
+  }
+
+}
